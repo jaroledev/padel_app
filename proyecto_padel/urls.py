0a3551176f4285ba1dd54a37762495f3ef5fff07
jaroledev <dawjarole@gmail.com>
2024-06-03 01:27:59 +0200

    borrar ruta actualizar reserva

--- proyecto_padel/urls.py
+++ proyecto_padel/urls.py
@@ -28,7 +28,6 @@ urlpatterns = [
     path('crear_reserva/', views.crear_reserva, name='crear_reserva'),
     path('reserva_pista/<int:pista_id>/', views.reserva_pista, name='reservaPista'),
     path('mis_reservas/', views.misReservas, name='misReservas'),
-    path('actualizar_reserva/<int:reserva_id>/', views.actualizarReserva, name='actualizarReserva'),
     path('delete/<int:reserva_id>/', views.delete_reserva, name='delete'),
     path('clubs_disponibles/', views.clubs_disponibles, name='clubsDisponibles'),
     path('usuario/', views.usuario, name='usuario')
